6bb962c95ca815748b669be69d0bd864c3cd3232
VICENTE0777 <vic.alarcona@duocuc.cl>
2026-05-21 17:38:38 -0400

    Fix: remove duplicate outputs

--- main.tf
+++ main.tf
@@ -2,31 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Instancia VM A
 resource "aws_instance" "vm_a" {
-  ami           = "ami-0fc5d935ebf8bc3bc"   # Ubuntu Server 22.04 LTS válido en us-east-1
+  ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "technova-vm-a"
-  }
+  tags = { Name = "technova-vm-a" }
 }
 
-# Instancia VM B
 resource "aws_instance" "vm_b" {
-  ami           = "ami-0fc5d935ebf8bc3bc"   # mismo AMI
+  ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "technova-vm-b"
-  }
-}
-
-# Outputs para mostrar IPs públicas
-output "vm_a_public_ip" {
-  value = aws_instance.vm_a.public_ip
-}
-
-output "vm_b_public_ip" {
-  value = aws_instance.vm_b.public_ip
+  tags = { Name = "technova-vm-b" }
 }
